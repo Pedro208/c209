4e8f447104781b9898707a667d40500e982d49fa
Pedro Augusto <pedrodaluzmota208@gmail.com>
2022-10-07 07:07:02 -0300

    video car detection

---
+++ projeto1/main.py
@@ -0,0 +1,31 @@
+import cv2  
+import numpy as np
+
+cap = cv2.VideoCapture('video.avi')
+
+if (cap.isOpened()== False):
+  print("Error opening video stream or file")
+
+car_cascade = cv2.CascadeClassifier('cars.xml')
+while(cap.isOpened()):
+    ret, frames = cap.read()
+      
+    # convert to gray scale of each frames
+    gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
+      
+  
+    # Detects cars of different sizes in the input image
+    cars = car_cascade.detectMultiScale(gray, 1.1, 1)
+      
+    # To draw a rectangle in each cars
+    for (x,y,w,h) in cars:
+        cv2.rectangle(frames,(x,y),(x+w,y+h),(0,0,255),2)
+  
+   # Display frames in a window 
+    cv2.imshow('video2', frames)
+      
+    # Wait for Esc key to stop
+    if cv2.waitKey(33) == 27:
+        break
+
+cv2.destroyAllWindows()
